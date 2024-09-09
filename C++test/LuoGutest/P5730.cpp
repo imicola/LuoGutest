59c89d26920df2a3e26bbb8a0c5887929ee95b57
imicola <imicola@outlook.com>
2024-09-09 12:00:49 +0800

    main

---
+++ C++test/LuoGutest/P5730.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string str;
+    getline(cin,str);
+    str = "\n";
+    getline(cin,str);
+    //uncomplete
+}
