ed5ceb6d5d303edc341854894a2ad23567560da6
imicola <imicola@outlook.com>
2024-09-10 21:36:27 +0800

    main

---
+++ C++test/LuoGutest/蓝桥杯Q5.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int prime(int a)
+{
+    if(a == 2)
+    {
+        return 1;
+    }
+    for (size_t i = 2; i*i <= a; i++)
+    {
+        if (a%i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int n = 0,sum = 0;
+    cin >> n;
+    int p = n;
+    for (size_t i = 2; i <= sqrt(n); i++)
+    {
+        if(prime(i) == 1)
+        {
+            while (p%i == 0)
+            {
+                p /= i;
+                sum += i;
+            }
+        }
+    }
+
+    if (n <= 5)
+    {
+        cout << n;
+    }
+    else
+    {
+        cout << sum;
+    }
+    return 0;
+}
