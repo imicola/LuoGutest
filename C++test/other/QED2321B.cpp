67736b00566990e348b01f053bdf6d494c605f8a
imicola <imicola@outlook.com>
2024-09-15 23:38:44 +0800

    不会写的题目增加了

---
+++ C++test/other/QED2321B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n*n + 1];
+    int arr1[n*n + 1];
+    for (size_t i = 1; i <= n*n + 1; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr+1,arr+n*n+2);
+    int q;
+    cin >> q;
+    for (size_t i = 0 ;i <= q; i++)
+    {
+        int x,k;
+        cin >> x >> k;
+
+    }
+    //uncomplete
+}
