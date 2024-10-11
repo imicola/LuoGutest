b0b7cbf63db9ab442387238ef936df26c8b036d4
imicola <imicola@outlook.com>
2024-10-11 21:08:27 +0800

    first sit and test

--- C++test/LuoGutest/P9226(baby).cpp
+++ C++test/LuoGutest/P9226(baby).cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n,t;
+    int n = 0,t = 0;
     cin >> n >>t;
     cout << t - n%t;
     return 0;
--- C++test/other/test/test3.cpp
+++ C++test/other/test/test3.cpp
@@ -1,22 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long fastpower(long long a , long long b)
-{
-    long long ans = 1;
-    while (b > 0)
-    {
-        if (b&1)
-        {
-            ans *= a;
-        }
-        a *= a;
-        b >>= 1;
-    }
-    return ans;
-}
-
 int main()
 {
-    cout << fastpower(2,8);
+    double e = 0;
+    double f = 0;
 }
