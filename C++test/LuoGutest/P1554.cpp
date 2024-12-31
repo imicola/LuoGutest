00ab7746c21d48232d5fd051ea53af5a5f6f4cfa
imicola <imicola@outlook.com>
2024-12-31 11:55:25 +0800

    添加 .gitignore 文件并优化 C++ 代码格式

--- C++test/LuoGutest/P1554.cpp
+++ C++test/LuoGutest/P1554.cpp
@@ -5,24 +5,19 @@ int arr[11];
 
 int main()
 {
-    int m,n = 0;
+    int m, n = 0;
     cin >> m >> n;
-    for (size_t i = m; i <= n; i++)
-    {
+    for (size_t i = m; i <= n; i++) {
         string temp = to_string(i);
-        for (size_t k = 0; k < temp.size(); k++)
-        {
+        for (size_t k = 0; k < temp.size(); k++) {
             arr[temp[k] - '0']++;
         }
         temp = {0};
     }
-    
-    for (size_t i = 0; i < 10; i++)
-    {
-        cout << arr[i] <<" ";
-    }
-    
 
+    for (size_t i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
