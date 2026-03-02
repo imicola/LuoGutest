f6c2363ff5ce6871ef0ecbdb44e5f4ac8446acd6
imicola <imicola@outlook.com>
2026-03-02 20:19:43 +0800

    老兵回归,但是之后大概率不会太倾向算法了估计（

---
+++ main/codeforces/normal test/2166C.cpp
@@ -0,0 +1,44 @@
+//#pragma GCC optimize(3)
+#include <bits/stdc++.h>
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+using namespace std;
+using i64 = long long;
+using i128 = __int128;
+using vint = vector<i64>;
+using vvint = vector<vint>;
+using vstr = vector<string>;
+using pii = pair<i64, i64>;
+using vpii = vector<pii>;
+template <typename T>
+using vec = vector<T>;
+const constexpr i64 MOD = 998244353;
+
+void solve()
+{
+    i64 n;
+    cin >> n;
+    vint v(n);
+    i64 cnt = 0;
+    for (auto &&i : v) {
+        cin >> i;
+    }
+    v.emplace_back(v[0]);
+    for (i64 i = 0; i < n; i++) {
+        cnt += max(v[i], v[i + 1]);
+    }
+    cnt -= *max_element(all(v));
+    cout << cnt << endl;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout << setiosflags(ios::fixed) << setprecision(2);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
